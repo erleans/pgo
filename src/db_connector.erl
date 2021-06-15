c971868cbac6ad389d6f03ef2a7afb0a1586a970
aruhn1 <85363537+aruhn1@users.noreply.github.com>
2021-06-15 15:52:56 +0530

    Update db_connector.erl

--- src/db_connector.erl
+++ src/db_connector.erl
@@ -1,10 +1,10 @@
 %% Feel free to use, reuse and abuse the code in this file.
 %%
 %% The purpose of the DB Connector is to have a standard interface for the rest of the app to communicate with PGO.
-%% This will make it easy to reflect any changes in the return type of PGO
+%% This will make it easy to reflect any changes in the return type of PGO (e.g. when it moved from returning tuples to maps)
 %% Or to replace another tool with PGO, or vice versa. 
-%% So the app calls functions defined in a "helper" file.
-%% And the helper file communicates with PGO via the connector. 
+%% So the app calls functions defined in a "helper" file. 
+%% And the helper file communicates with PGO via the connector. So in case of any changes with PGO, only this connector file needs to be updated, and not every pgo:query call.
 
 %% @doc PGO connector
 
